1aa123a6e4bcaf5d50984f0591d3dbd45bcc393f
Kwok Yee Chu <k.y.chu96@gmail.com>
2018-11-25 20:12:56 +0000

    added an extra state

--- Main.cpp
+++ Main.cpp
@@ -6,43 +6,50 @@
 
 using namespace std;
 
-#define STATES 4
+#define STATES 5
 
 int main()
 {
+	typedef int ** iMatrix;
+	typedef double ** dMatrix;
+
 	// Transition arrays
-	int sunnyTrans[STATES] = { 00, 01, 02, 03 };
-	int rainingTrans[STATES] = { 11, 10, 12, 13 };
-	int windyTrans[STATES] = { 22, 20, 21, 23 };
-	int stormingTrans[STATES] = { 33, 30, 31, 32 };
+	int sunnyTrans[STATES] = { 00, 01, 02, 03, 04 };
+	int rainingTrans[STATES] = { 11, 10, 12, 13, 14 };
+	int windyTrans[STATES] = { 22, 20, 21, 23, 24 };
+	int stormingTrans[STATES] = { 33, 30, 31, 32, 34 };
+	int snowingTrans[STATES] = { 44, 40, 41, 42, 43 };
 
 	int* transArray[STATES];
 	transArray[0] = sunnyTrans;
 	transArray[1] = rainingTrans;
 	transArray[2] = windyTrans;
 	transArray[3] = stormingTrans;
+	transArray[4] = snowingTrans;
 
 	// Transitions matrix
-	int** trans = new int*[STATES];
+	iMatrix trans = new int*[STATES];
 	for (size_t i = 0; i < STATES; i++)
 	{
 		trans[i] = transArray[i];
 	}
 
 	// Probability arrays
-	double sunnyProbs[STATES] = { 0.5, 0.2, 0.2, 0.1 };
-	double rainingProbs[STATES] = { 0.3, 0.2, 0.3, 0.2 };
-	double windyProbs[STATES] = { 0.3, 0.2, 0.3, 0.2 };
-	double stormingProbs[STATES] = { 0.4, 0.1, 0.3, 0.2 };
+	double sunnyProbs[STATES] = { 0.5, 0.2, 0.2, 0.1, 0 };
+	double rainingProbs[STATES] = { 0.3, 0.2, 0.2, 0.1, 0.2 };
+	double windyProbs[STATES] = { 0.3, 0.1, 0.2, 0.1, 0.3 };
+	double stormingProbs[STATES] = { 0.4, 0.1, 0.3, 0.2, 0 };
+	double snowingProbs[STATES] = { 0.5, 0, 0.2, 0.2, 0.1 };
 
 	double* probsArray[STATES];
 	probsArray[0] = sunnyProbs;
 	probsArray[1] = rainingProbs;
 	probsArray[2] = windyProbs;
 	probsArray[3] = stormingProbs;
+	probsArray[4] = snowingProbs;
 
 	// Probabilities matrix
-	double** probs = new double*[STATES];
+	dMatrix probs = new double*[STATES];
 	for (size_t i = 0; i < STATES; i++)
 	{
 		probs[i] = probsArray[i];
@@ -57,6 +64,7 @@ int main()
 	int stateTwoCount = 0;
 	int stateThreeCount = 0;
 	int stateFourCount = 0;
+	int stateFiveCount = 0;
 
 	// Total loops
 	int loops = 0;
@@ -87,10 +95,14 @@ int main()
 			currentState = "STORMING";
 			stateFourCount++;
 			break;
+		case 4:
+			currentState = "SNOWING";
+			stateFiveCount++;
+			break;
 		}
 		cout << "CURRENT STATE > " << currentState << endl; // " Generated Random: " << getRandom() << endl;
 		if(loops % 5 == 0)
-			cout << "Sunny: " << stateOneCount << ", Rainy: " << stateTwoCount << ", Windy: " << stateThreeCount << ", Storming: " << stateFourCount << endl;
+			cout << "Sunny: " << stateOneCount << ", Rainy: " << stateTwoCount << ", Windy: " << stateThreeCount << ", Storming: " << stateFourCount << ", Snowing: " << stateFiveCount << endl;
 	}
 
 	cin.get();
